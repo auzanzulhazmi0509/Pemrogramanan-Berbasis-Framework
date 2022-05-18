3fb17f31ed023895ab2b17e2eaeb948a845ffb25
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2022-05-18 09:58:14 +0700

    edit config pada firebase

---
+++ Modul_11/modul-11/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAj0wV8dIm3_N0r5cXoUB57WRfubBryhPc",
+    authDomain: "modul11-9798f.firebaseapp.com",
+    projectId: "modul11-9798f",
+    storageBucket: "modul11-9798f.appspot.com",
+    messagingSenderId: "346567646862",
+    appId: "1:346567646862:web:311452cb98484d0e38b3df",
+    measurementId: "G-SKNJK4YD5M"
+
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
