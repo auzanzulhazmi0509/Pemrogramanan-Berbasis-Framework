83a28427014cd687f23f7daa0492ab1573446f54
auzanzulhazmi0509 <zulhazmiauzan@gmail.com>
2022-05-24 21:21:38 +0700

    Modul-11 Firebase Login

--- Modul_11/modul-11/src/actions/auth.js
+++ Modul_11/modul-11/src/actions/auth.js
@@ -1,101 +1,91 @@
-import { myFirebase } from '../firebase/firebase';
+import { myFirebase } from "../firebase/firebase";
+import {
+    onAuthStateChanged,
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut
+} from 'firebase/auth';
 
 export const LOGIN_REQUEST = "LOGIN_REQUEST";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
-
 export const LOGOUT_REQUEST = "LOGOUT_REQUEST";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
 export const LOGOUT_FAILURE = "LOGOUT_FAILURE";
+export const VERIFY_REQUEST = "VERIFY_REQUEST";
+export const VERIFY_SUCCESS = "VERIFY_SUCCESS";
 
-export const VERTIFY_REQUEST = "VERTIFY_REQUEST";
-export const VERTIFY_SUCCESS = "VERTIFY_SUCCESS";
+const auth = getAuth(myFirebase)
 
 const requestLogin = () => {
     return {
         type: LOGIN_REQUEST
     };
 };
-
 const receiveLogin = user => {
     return {
         type: LOGIN_SUCCESS,
         user
     };
 };
-
 const loginError = () => {
     return {
         type: LOGIN_FAILURE
     };
 };
-
 const requestLogout = () => {
     return {
         type: LOGOUT_REQUEST
     };
 };
-
 const receiveLogout = () => {
     return {
-        type: LOGOUT_SUCCESS,
+        type: LOGOUT_SUCCESS
     };
 };
-
 const logoutError = () => {
     return {
         type: LOGOUT_FAILURE
     };
 };
-
-const vertifyRequest = () => {
+const verifyRequest = () => {
     return {
-        type: VERTIFY_REQUEST
+        type: VERIFY_REQUEST
     };
 };
-
-const vertifySuccess = () => {
+const verifySuccess = () => {
     return {
-        type: VERTIFY_SUCCESS
+        type: VERIFY_SUCCESS
     };
 };
-
 export const loginUser = (email, password) => dispatch => {
     dispatch(requestLogin());
-    myFirebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then(user => {
-            dispatch(loginUser(user));
+            dispatch(receiveLogin(user));
         })
         .catch(error => {
+            //Do something with the error if you want!
             dispatch(loginError());
-        })
-}
-
+        });
+};
 export const logoutUser = () => dispatch => {
     dispatch(requestLogout());
-    myFirebase
-        .auth()
-        .signOut()
-        .then(res => {
+        signOut(auth)
+        .then(() => {
             dispatch(receiveLogout());
         })
         .catch(error => {
+            //Do something with the error if you want!
             dispatch(logoutError());
-        })
-}
-
-export const vertifyAuth = () => dispatch => {
-    dispatch(vertifyRequest());
-    myFirebase.auth().onAuthStateChanged(user => {
-        if (user != null) {
-            dispatch(receiveLogin());
+        });
+};
+export const verifyAuth = () => dispatch => {
+    dispatch(verifyRequest());
+    onAuthStateChanged(auth, (user) => {
+        if (user !== null) {
+            dispatch(receiveLogin(user));
         }
-        dispatch(vertifySuccess());
-    })
-}
-
-
-
-
+        dispatch(verifySuccess());
+    });
+};
--- Modul_11/modul-11/src/firebase/firebase.js
+++ Modul_11/modul-11/src/firebase/firebase.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyAj0wV8dIm3_N0r5cXoUB57WRfubBryhPc",
